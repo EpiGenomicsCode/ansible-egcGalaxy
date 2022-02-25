857eac6d61c0ea79b99fc0602b99b57185dc2154
William Lai <kai.ming.lai@gmail.com>
2022-02-25 15:28:10 -0500

    scATAC script updates

--- galaxy/files/galaxy/tools/scATAC_decell.py
+++ galaxy/files/galaxy/tools/scATAC_decell.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import collections
+import optparse
 
-min_reads = int(sys.argv[1])
-dirt = sys.argv[2]
+def main():
+    """ main function """
+    parser = optparse.OptionParser(usage='%prog [-h] [-m Min reads per cell]',
+                                   description='Parse individual cells from scATAC-seq data.')
+    parser.add_option('-m',
+                      dest="min",
+                      help='Minimum number of reads',
+                      type="int"
+                      )
 
-prev_barcode = ""
-instances = []
-heads = []
-for line in sys.stdin:
-    if line.split()[0]=='@SQ' or line.split()[0]=='@PG':
-        heads.append(line)
-    cur_barcode = line.strip().split()[0].split(':')[0]
-    if cur_barcode != prev_barcode:
-        if(len(instances) >= min_reads):
-            with open(dirt + '/' +  prev_barcode + '.sam', 'w') as fout:
-                for elem in heads:
-                    fout.write(elem)
-                for elem in instances:
-                    fout.write(elem)
-        prev_barcode = cur_barcode
-        instances = []
-        instances.append(line)
-    else:
-        instances.append(line)
+    if len(sys.argv) < 2:
+        parser.print_help()
+        exit('error: too few arguments')
+
+    args = parser.parse_args()[0]
+    min_reads = args.min
+
+    os.mkdir('output')
+
+    prev_barcode = ""
+    instances = []
+    heads = []
+    for line in sys.stdin:
+        if line.split()[0]=='@SQ' or line.split()[0]=='@PG':
+            heads.append(line)
+        else:
+            cur_barcode = line.strip().split()[0].split(':')[0]
+            if cur_barcode != prev_barcode:
+                if(len(instances) >= min_reads) and prev_barcode != "":
+                    with open(os.path.join('output', prev_barcode + '.sam'), 'w') as fout:
+                        for elem in heads:
+                            fout.write(elem)
+                        for elem in instances:
+                            fout.write(elem)
+                prev_barcode = cur_barcode
+                instances = []
+                instances.append(line)
+            else:
+                instances.append(line)
+
+if __name__ == '__main__':
+    main()
