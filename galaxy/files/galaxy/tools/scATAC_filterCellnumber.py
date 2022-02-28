8e564a52decb91d6fd8f3cf0f0f8b988e6961d3e
William Lai <kai.ming.lai@gmail.com>
2022-02-28 12:01:21 -0500

    Add additional scATAC-seq tools

--- galaxy/files/galaxy/tools/scATAC_filterCellnumber.py
+++ galaxy/files/galaxy/tools/scATAC_filterCellnumber.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 
 import sys
+import optparse
 
 def main():
+    """ main function """
+    parser = optparse.OptionParser(usage='%prog [-h] [-m Minimum Reads] [-f Barcode Frequency]',
+                                   description='Filter BAM file, retaining cells with minimum number of associated reads.')
+    parser.add_option('-m',
+        dest="min_reads",
+        help='Minimum reads to retain cell',
+        type="int",
+    )
+    parser.add_option('-f',
+        dest="freq",
+        help='Barcode frequency'
+    )
+
+    if len(sys.argv) < 4:
+        parser.print_help()
+        exit('error: too few arguments')
+
+    args = parser.parse_args()[0]
+    min_cov = args.min_reads
+    fname = args.freq
+
     barcode_list = []
-    min_cov = int(sys.argv[1])
-    fname = sys.argv[2]
     # load in the barcode
     with open(fname) as fin:
         for line in fin:
@@ -17,7 +37,7 @@ def main():
         # head of bam file
         if line[0] == '@':
             try:
-                print line,
+                print(line,end='')
             except IOError:
                 try:
                     sys.stdout.close()
@@ -32,7 +52,7 @@ def main():
         barcode_cur = line.split()[0].split(":")[0]
         if barcode_cur in barcode_list:
             try:
-                print line,
+                print(line,end='')
             except IOError:
                 try:
                     sys.stdout.close()
